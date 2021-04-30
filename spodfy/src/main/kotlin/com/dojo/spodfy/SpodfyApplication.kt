25550bbd099c791f232d72a470078c682c0db064
Miles Morales <pgsilva0698@gmail.com>
2021-04-29 21:42:28 -0300

    fix

--- spodfy/src/main/kotlin/com/dojo/spodfy/SpodfyApplication.kt
+++ spodfy/src/main/kotlin/com/dojo/spodfy/SpodfyApplication.kt
@@ -4,7 +4,6 @@ import com.dojo.spodfy.configuration.LogInterceptor
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.cloud.openfeign.EnableFeignClients
 import org.springframework.context.annotation.Configuration
 import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
@@ -12,7 +11,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @SpringBootApplication
 @EnableScheduling
-@EnableFeignClients
 class SpodfyApplication
 
 fun main(args: Array<String>) {
